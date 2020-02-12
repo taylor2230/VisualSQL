cacdb420f3ff566de0d0cf0ea0a7ae01d948629c
taylor2230 <60805326+taylor2230@users.noreply.github.com>
2020-02-11 20:35:55 -0600

    Index page formatting

--- index.php
+++ index.php
@@ -5,7 +5,20 @@
     <title>VisualSQL | Login</title>
 </head>
 <body>
-
+<div id="top"></div>
+<div id="visuals">
+    <h1 class="title" id="sitename"><a href="index.php">Visual SQL</a></h1>
+    <img id="logo" alt="" src="">
+</div>
+<div id="credentials">
+    <form action="">
+        <label for="loginid">LOGIN</label><br>
+        <input type="text" id="loginid" name="loginid" required><br><br>
+        <label for="password">PASSWORD</label><br>
+        <input type="password" id="password" name="password" required><br><br>
+        <input type="submit" value="Submit">
+    </form>
+</div>
 </body>
 </html>
 <?php
